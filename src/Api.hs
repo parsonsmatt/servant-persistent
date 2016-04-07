8477aaad464377d815465350aa6e8ba76f293535
Matt Parsons <parsonsmatt@gmail.com>
2016-04-07 15:16:31 -0400

    update to servant-0.5

--- src/Api.hs
+++ src/Api.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Api where
 
-import Control.Monad.Reader         (ReaderT, runReaderT, lift)
-import Control.Monad.Trans.Either   (EitherT, left)
+import Control.Monad.Except
+import Control.Monad.Reader         (ReaderT, runReaderT)
+import Control.Monad.Reader.Class
 import Network.Wai                  (Application)
 import Database.Persist.Postgresql  (insert, selectList, Entity(..)
-                                    ,fromSqlKey, (==.))
+                                    ,fromSqlKey, (==.), selectFirst)
 import Data.Int                     (Int64)
 import Servant
+import Servant.Server
 
 import Config    (Config(..))
 import Models -- (Person, userToPerson, EntityField(UserName))
@@ -19,7 +22,11 @@ type PersonAPI =
     :<|> "users" :> Capture "name" String :> Get '[JSON] Person
     :<|> "users" :> ReqBody '[JSON] Person :> Post '[JSON] Int64
 
-type AppM = ReaderT Config (EitherT ServantErr IO)
+newtype App a 
+    = App 
+    { runApp :: ReaderT Config (ExceptT ServantErr IO) a 
+    } deriving ( Functor, Applicative, Monad, MonadReader Config, 
+                 MonadError ServantErr, MonadIO)
 
 userAPI :: Proxy PersonAPI
 userAPI = Proxy
@@ -28,29 +35,29 @@ app :: Config -> Application
 app cfg = serve userAPI (readerServer cfg)
 
 readerServer :: Config -> Server PersonAPI
-readerServer cfg = enter (readerToEither cfg) server
+readerServer cfg = enter (convertApp cfg) server
 
-readerToEither :: Config -> AppM :~> EitherT ServantErr IO
-readerToEither cfg = Nat $ \x -> runReaderT x cfg
+convertApp :: Config -> App :~> ExceptT ServantErr IO
+convertApp cfg = Nat (flip runReaderT cfg . runApp)
 
-server :: ServerT PersonAPI AppM
+server :: ServerT PersonAPI App
 server = allPersons :<|> singlePerson :<|> createPerson
 
-allPersons :: AppM [Person]
+allPersons :: App [Person]
 allPersons = do
-    users <- runDb $ selectList [] []
-    let people = map (\(Entity _ y) -> userToPerson y) users
+    users <- runDb (selectList [] [])
+    let people = map (userToPerson . entityVal) users
     return people
 
-singlePerson :: String -> AppM Person
+singlePerson :: String -> App Person
 singlePerson str = do
-    users <- runDb $ selectList [UserName ==. str] []
-    let list = map (\(Entity _ y) -> userToPerson y) users
-    case list of
-         []     -> lift $ left err404
-         (x:xs) -> return x
+    maybeUser <- runDb (selectFirst [UserName ==. str] [])
+    let maybePerson = fmap (userToPerson . entityVal) maybeUser
+    case maybePerson of
+         Nothing     -> throwError err404
+         Just person -> return person
 
-createPerson :: Person -> AppM Int64
+createPerson :: Person -> App Int64
 createPerson p = do
-    newPerson <- runDb $ insert $ User (name p) (email p)
+    newPerson <- runDb (insert (User (name p) (email p)))
     return $ fromSqlKey newPerson
