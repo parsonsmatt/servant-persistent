db6a7e97598738ea064dcedddbfb5727f9e965ed
Steven Leiva <leiva.steven@gmail.com>
2017-09-14 09:32:57 -0400

    Use 4 spaces for indentation in user db specs.

--- test/UserDbSpec.hs
+++ test/UserDbSpec.hs
@@ -1,63 +1,60 @@
-{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE TypeOperators #-}
 
 module UserDbSpec where
 
-import           Test.Hspec
-import           Test.QuickCheck
+import Test.Hspec
+import Test.QuickCheck
 
-import           Control.Monad.Except      (runExceptT)
-import           Control.Monad.Reader      (runReaderT)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.Reader (runReaderT)
 
-import           Servant
-import           Database.Persist.Types      (Filter)
-import           Database.Persist.Sql        (ConnectionPool, transactionUndo)
-import           Database.Persist.Postgresql (Entity (..), fromSqlKey, insert,
-                                              selectFirst, selectList, deleteWhere,
-                                              (==.), runSqlPool)
+import Database.Persist.Postgresql
+       (Entity(..), (==.), deleteWhere, fromSqlKey, insert, runSqlPool,
+        selectFirst, selectList)
+import Database.Persist.Sql (ConnectionPool, transactionUndo)
+import Database.Persist.Types (Filter)
+import Servant
 
-import           Config (App (..), Config (..), Environment (..), makePool)
-import           Models
-import           Api.User
-
+import Api.User
+import Config (App(..), Config(..), Environment(..), makePool)
+import Models
 
 runAppToIO :: Config -> App a -> IO a
 runAppToIO config app = do
-  result <- runExceptT $ runReaderT (runApp app) config
-  case result of
-    Left err -> error $ show err
-    Right a  -> return a
+    result <- runExceptT $ runReaderT (runApp app) config
+    case result of
+        Left err -> error $ show err
+        Right a -> return a
 
 setupTeardown :: (Config -> IO a) -> IO ()
 setupTeardown runTestsWith = do
-  pool <- makePool Test
-  migrateDb pool
-  runTestsWith $ Config { getPool = pool, getEnv = Test }
-  cleanDb pool
+    pool <- makePool Test
+    migrateDb pool
+    runTestsWith $ Config {getPool = pool, getEnv = Test}
+    cleanDb pool
   where
     migrateDb :: ConnectionPool -> IO ()
     migrateDb pool = runSqlPool doMigrations pool
-
     cleanDb :: ConnectionPool -> IO ()
     cleanDb = deleteAllUsers
-
     deleteAllUsers :: ConnectionPool -> IO ()
     deleteAllUsers pool = do
-      flip runSqlPool pool $ do
-        deleteWhere ([] :: [Filter User])
-
+        flip runSqlPool pool $ do deleteWhere ([] :: [Filter User])
 
 -- for more detail, see `src/Config.hs`, but this assumes you have...
 --   1. a Postgres `test` user
 --   2. a `perservant-test` DB
 spec :: Spec
-spec = around setupTeardown $ do
-  describe "User" $ do
-    it "singleUser fetches User by name" $ \config -> do
-      let user = User "username" "email"
-      dbUser <- runAppToIO config $ do
-        runDb $ insert user
-        Entity _ user <- singleUser "username"
-        return user
-      dbUser `shouldBe` user
+spec =
+    around setupTeardown $ do
+        describe "User" $ do
+            it "singleUser fetches User by name" $ \config -> do
+                let user = User "username" "email"
+                dbUser <-
+                    runAppToIO config $ do
+                        runDb $ insert user
+                        Entity _ user <- singleUser "username"
+                        return user
+                dbUser `shouldBe` user
